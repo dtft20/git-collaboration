8bbf8905622cf8ed0ed21d159e1906d98657143a
dtft20 <dtft20@durham.ac.uk>
2025-11-04 10:02:42 +0000

    Added text content attribute to colour switch

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,8 +1,11 @@
 document.getElementById("colourSwitch").addEventListener("click", () => {
-    if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+    if (document.documentElement.getAttribute("data-bs-theme") == null 
+    || document.documentElement.getAttribute("data-bs-theme") == "dark") {
         document.documentElement.setAttribute("data-bs-theme", "light");
+        document.getElementById("colourSwitch").textContent = "Switch to dark mode"
     }
     else if (document.documentElement.getAttribute("data-bs-theme") == "light") {
         document.documentElement.setAttribute("data-bs-theme", "dark");
+        document.getElementById("colourSwitch").textContent = "Switch to light mode"
     }
 })
